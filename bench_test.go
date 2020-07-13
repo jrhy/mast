0c6dde4a3e8d304f6b11b4dd362a7e9292b91eb0
jrhy <jrhy>
2020-07-13 09:26:31 -0700

    add a benchmark for exerciser

--- bench_test.go
+++ bench_test.go
@@ -1,7 +1,12 @@
 package mast
 
 import (
+	"bytes"
 	"testing"
+
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/commands"
+	"github.com/stretchr/testify/require"
 )
 
 func benchmarkStdMapInsert(factor int, b *testing.B) {
@@ -14,10 +19,10 @@ func benchmarkStdMapInsert(factor int, b *testing.B) {
 func BenchmarkStdMapInsert1(b *testing.B)    { benchmarkStdMapInsert(1, b) }
 func BenchmarkStdMapInsert10(b *testing.B)   { benchmarkStdMapInsert(10, b) }
 func BenchmarkStdMapInsert100(b *testing.B)  { benchmarkStdMapInsert(100, b) }
-func BenchmarkStdMapInsert1k(b *testing.B)   { benchmarkStdMapInsert(1000, b) }
-func BenchmarkStdMapInsert10k(b *testing.B)  { benchmarkStdMapInsert(10000, b) }
-func BenchmarkStdMapInsert100k(b *testing.B) { benchmarkStdMapInsert(100000, b) }
-func BenchmarkStdMapInsert1m(b *testing.B)   { benchmarkStdMapInsert(1000000, b) }
+func BenchmarkStdMapInsert1k(b *testing.B)   { benchmarkStdMapInsert(1_000, b) }
+func BenchmarkStdMapInsert10k(b *testing.B)  { benchmarkStdMapInsert(10_000, b) }
+func BenchmarkStdMapInsert100k(b *testing.B) { benchmarkStdMapInsert(100_000, b) }
+func BenchmarkStdMapInsert1m(b *testing.B)   { benchmarkStdMapInsert(1_000_000, b) }
 
 func benchmarkStdMapGet(factor int, b *testing.B) {
 	m := map[int]int{}
@@ -34,10 +39,10 @@ func benchmarkStdMapGet(factor int, b *testing.B) {
 func BenchmarkStdMapGet1(b *testing.B)    { benchmarkStdMapGet(1, b) }
 func BenchmarkStdMapGet10(b *testing.B)   { benchmarkStdMapGet(10, b) }
 func BenchmarkStdMapGet100(b *testing.B)  { benchmarkStdMapGet(100, b) }
-func BenchmarkStdMapGet1k(b *testing.B)   { benchmarkStdMapGet(1000, b) }
-func BenchmarkStdMapGet10k(b *testing.B)  { benchmarkStdMapGet(10000, b) }
-func BenchmarkStdMapGet100k(b *testing.B) { benchmarkStdMapGet(100000, b) }
-func BenchmarkStdMapGet1m(b *testing.B)   { benchmarkStdMapGet(1000000, b) }
+func BenchmarkStdMapGet1k(b *testing.B)   { benchmarkStdMapGet(1_000, b) }
+func BenchmarkStdMapGet10k(b *testing.B)  { benchmarkStdMapGet(10_000, b) }
+func BenchmarkStdMapGet100k(b *testing.B) { benchmarkStdMapGet(100_000, b) }
+func BenchmarkStdMapGet1m(b *testing.B)   { benchmarkStdMapGet(1_000_000, b) }
 
 func benchmarkInsert(factor int, b *testing.B) {
 	m := newTestTree(0, "")
@@ -49,10 +54,10 @@ func benchmarkInsert(factor int, b *testing.B) {
 func BenchmarkInsert1(b *testing.B)    { benchmarkInsert(1, b) }
 func BenchmarkInsert10(b *testing.B)   { benchmarkInsert(10, b) }
 func BenchmarkInsert100(b *testing.B)  { benchmarkInsert(100, b) }
-func BenchmarkInsert1k(b *testing.B)   { benchmarkInsert(1000, b) }
-func BenchmarkInsert10k(b *testing.B)  { benchmarkInsert(10000, b) }
-func BenchmarkInsert100k(b *testing.B) { benchmarkInsert(100000, b) }
-func BenchmarkInsert1m(b *testing.B)   { benchmarkInsert(1000000, b) }
+func BenchmarkInsert1k(b *testing.B)   { benchmarkInsert(1_000, b) }
+func BenchmarkInsert10k(b *testing.B)  { benchmarkInsert(10_000, b) }
+func BenchmarkInsert100k(b *testing.B) { benchmarkInsert(100_000, b) }
+func BenchmarkInsert1m(b *testing.B)   { benchmarkInsert(1_000_000, b) }
 
 func benchmarkGet(factor int, b *testing.B) {
 	m := newTestTree(0, "")
@@ -70,7 +75,18 @@ func benchmarkGet(factor int, b *testing.B) {
 func BenchmarkGet1(b *testing.B)    { benchmarkGet(1, b) }
 func BenchmarkGet10(b *testing.B)   { benchmarkGet(10, b) }
 func BenchmarkGet100(b *testing.B)  { benchmarkGet(100, b) }
-func BenchmarkGet1k(b *testing.B)   { benchmarkGet(1000, b) }
-func BenchmarkGet10k(b *testing.B)  { benchmarkGet(10000, b) }
-func BenchmarkGet100k(b *testing.B) { benchmarkGet(100000, b) }
-func BenchmarkGet1m(b *testing.B)   { benchmarkGet(1000000, b) }
+func BenchmarkGet1k(b *testing.B)   { benchmarkGet(1_000, b) }
+func BenchmarkGet10k(b *testing.B)  { benchmarkGet(10_000, b) }
+func BenchmarkGet100k(b *testing.B) { benchmarkGet(100_000, b) }
+func BenchmarkGet1m(b *testing.B)   { benchmarkGet(1_000_000, b) }
+
+func BenchmarkExerciser(b *testing.B) {
+	parameters := gopter.DefaultTestParametersWithSeed(1593228262585360000)
+	parameters.MaxSize = 2048
+	parameters.MinSuccessfulTests = b.N
+	properties := gopter.NewProperties(parameters)
+	properties.Property("mast exerciser", commands.Prop(mastCommands))
+	out := bytes.NewBuffer(nil)
+	reporter := gopter.NewFormatedReporter(false, 98, out)
+	require.True(b, properties.Run(reporter))
+}
